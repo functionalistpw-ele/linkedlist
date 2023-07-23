1322c81876025ceb1b2fa6c9d3289765510af2c5
functionalistpw-ele <138759145+functionalistpw-ele@users.noreply.github.com>
2023-07-23 07:59:32 +0700

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+void printList(Node* n) {
+    while (n != nullptr) {
+        std::cout << n->data << " ";
+        n = n->next;
+    }
+}
+
+int main() {
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = nullptr;
+
+    printList(head);
+    return 0;
+}
